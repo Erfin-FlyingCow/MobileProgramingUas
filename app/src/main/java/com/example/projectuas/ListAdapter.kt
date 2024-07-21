2af9a36513ee161bb2244f778c571aeb666ae53d
Erfin-FlyingCow <noufalirfan02@gmail.com>
2024-07-21 10:49:41 +0700

    add adapter

---
+++ app/src/main/java/com/example/projectuas/ListAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.projectuas
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ListAdapter(private val lokasilist: List<ListHead>) : RecyclerView.Adapter<ListAdapter.Listviewholder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Listviewholder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return Listviewholder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: Listviewholder, position: Int) {
+        val currentItem = lokasilist[position]
+        holder.judul.text = currentItem.judul
+        holder.lokasi.text = currentItem.lokasi
+
+    }
+
+    override fun getItemCount() = lokasilist.size
+
+    class Listviewholder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val judul: TextView = itemView.findViewById(R.id.judul)
+        val lokasi: TextView = itemView.findViewById(R.id.lokasi)
+    }
+}
---
+++ app/src/main/java/com/example/projectuas/ListHead.kt
@@ -0,0 +1,3 @@
+package com.example.projectuas
+
+data class ListHead(var judul : String, var lokasi : String)
